0a02a4720cedc5266faf7e1629f20f68c7744b1e
FelipeAGomes <felipeagomes2202@gmail.com>
2023-04-08 22:06:15 +0100

    Assets pages

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,9 +1,9 @@
 <template>
-    <h1>Home</h1>
-    <h2>Welcome {{ user.firstName }} {{ user.lastName }}</h2>
-    <h2>Your id: {{ user.id }}</h2>
-    <h2>Your email: {{ user.email }}</h2>
-    <h2>Your profit is : R$ {{ user.profitTotal }}</h2>
+    <h2>Home</h2>
+    <h4>Welcome {{ user.firstName }} {{ user.lastName }}</h4>
+    <h4>Your id: {{ user.id }}</h4>
+    <h4>Your email: {{ user.email }}</h4>
+    <h4>Your profit is : ${{ user.profitTotal }}</h4>
 </template>
 
 <script setup lang="ts">
